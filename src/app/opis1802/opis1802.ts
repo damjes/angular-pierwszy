c415a65be19152866227767a1d65c5baa5ab53c9
Damjes <git@damj.es>
2025-10-21 13:42:13 +0200

    Dodano komponent opis1802

---
+++ src/app/opis1802/opis1802.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opis1802',
+  imports: [],
+  templateUrl: './opis1802.html',
+  styleUrl: './opis1802.sass'
+})
+export class Opis1802 {
+
+}
