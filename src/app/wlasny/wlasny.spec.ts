b4792ed68f1fd2781ed647df5a5caeaadae04286
Damjes <git@damj.es>
2025-10-21 12:36:56 +0200

    Dodałem własny komponent

---
+++ src/app/wlasny/wlasny.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Wlasny } from './wlasny';
+
+describe('Wlasny', () => {
+  let component: Wlasny;
+  let fixture: ComponentFixture<Wlasny>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Wlasny]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Wlasny);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/wlasny/wlasny.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wlasny',
+  imports: [],
+  templateUrl: './wlasny.html',
+  styleUrl: './wlasny.sass'
+})
+export class Wlasny {
+
+}
