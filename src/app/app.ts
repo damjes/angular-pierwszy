8c62a0dce027ad84c10decfd9505a4b1e1cc2fe1
Damjes <git@damj.es>
2025-10-21 12:40:09 +0200

    Komponent wlasny trochę działa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Wlasny } from "./wlasny/wlasny";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Wlasny],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
