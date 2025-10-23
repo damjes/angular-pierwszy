4ceccd9469b2f25c16351c54726ba3c43a3a23c3
Damjes <git@damj.es>
2025-10-23 11:29:37 +0200

    Dzisiejsze zadanko zrobione

--- src/app/formularzowy/formularzowy.ts
+++ src/app/formularzowy/formularzowy.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formularzowy',
-  imports: [FormsModule],
+  imports: [NgClass, NgStyle, FormsModule],
   templateUrl: './formularzowy.html',
   styleUrl: './formularzowy.sass',
 })
 export class Formularzowy {
   imie: string = 'Damjes';
+  ileRazy: number = 0;
+  kolorTla: string = 'white';
+  wielokrotnieKlikniete: boolean = false;
+
+  zwieksz() {
+    this.ileRazy++;
+    if (this.ileRazy % 5 === 0) {
+      this.wielokrotnieKlikniete = true;
+    }
+  }
+
+  zmienKolor() {
+    this.kolorTla = this.kolorTla === 'white' ? 'lightblue' : 'white';
+  }
 }
