92d5ad8cadfce8736f74ef8d36752ad0741b6056
Damjes <git@damj.es>
2025-10-23 11:10:54 +0200

    Dodano komponent formularzowy

---
+++ src/app/formularzowy/formularzowy.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Formularzowy } from './formularzowy';
+
+describe('Formularzowy', () => {
+  let component: Formularzowy;
+  let fixture: ComponentFixture<Formularzowy>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Formularzowy]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Formularzowy);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/formularzowy/formularzowy.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularzowy',
+  imports: [],
+  templateUrl: './formularzowy.html',
+  styleUrl: './formularzowy.sass',
+})
+export class Formularzowy {
+
+}
